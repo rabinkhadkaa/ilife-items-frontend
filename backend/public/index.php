63de4d8401046a394d55acaf68ec3aa9f4564ca9
unknown <rabin.khadka0@gmail.com>
2025-12-06 22:10:05 -0500

    Made clickable

--- backend/public/index.php
+++ backend/public/index.php
@@ -3,35 +3,33 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
-use Illuminate\Database\Capsule\Manager as Capsule;
 
-// Load .env
 $dotenv = Dotenv::createImmutable(__DIR__ . '/..');
 $dotenv->safeLoad();
 
-// Slim app
 $app = AppFactory::create();
 
-$app->addBodyParsingMiddleware();
+// 1: Routing middleware FIRST
 $app->addRoutingMiddleware();
 
-// CORS middleware
+// 2: CORS middleware NEXT (with OPTIONS in headers)
 $app->add(function ($request, $handler) {
     $response = $handler->handle($request);
+
     return $response
         ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+        ->withHeader('Access-Control-Allow-Credentials', 'true');
 });
 
-// Handle preflight OPTIONS requests
-$app->options('/{routes:.+}', function ($request, $response, $args) {
+// 3: Catch ALL OPTIONS for CORS preflight
+$app->options('/{routes:.+}', function ($request, $response) {
     return $response;
 });
-// Load database setup
-(require __DIR__ . '/../src/database.php')();
 
-// Load routes
+// Load DB + Routes
+(require __DIR__ . '/../src/database.php')();
 (require __DIR__ . '/../src/routes.php')($app);
 
 $app->run();
