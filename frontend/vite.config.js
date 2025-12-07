e9b3c542a82ccc52676aa1eb131ec802829e5ced
unknown <rabin.khadka0@gmail.com>
2025-12-06 23:05:06 -0500

    Made clickable

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
+    historyApiFallback: true,   // 🔥 Needed to support refresh on /item/1
     proxy: {
-      '/api': 'http://localhost:8081' // Slim backend URL
+      '/api': 'http://localhost:8081' // Slim backend URL (for local dev)
     }
+  },
+
+  preview: {
+    historyApiFallback: true   // Also required when running `vite preview`
   }
-})
+});
